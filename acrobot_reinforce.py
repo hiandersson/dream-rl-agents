47387eab7b1a7fbdb261b857e7487e5fbf6c3da9
Hans Andersson <hi.andersson@gmail.com>
2019-10-11 19:44:23 +0200

    run experiment acrobot

---
+++ acrobot_reinforce.py
@@ -0,0 +1,56 @@
+from run_experiment_helper import *
+from Agents.REINFORCE import REINFORCEAgent
+
+class AcrobotREINFORCE(RunExperiment):
+    def __init__(self):
+        RunExperiment.__init__(self, "Acrobot-v1")
+
+    def get_agent(self, config):
+        return REINFORCEAgent(config)
+
+    def get_agent_train_config(self, env):
+        return AgentConfig(
+            env=env,
+            n_episodes=300, 
+            target_average=-100,
+            max_t=200,
+            gamma=0.92,
+            fc1_units=512,
+            learning_rate=0.002)
+
+    def get_agent_evolve_config(self, env, genome, episodes):
+        return AgentConfig(
+            env=env,
+            device="cpu",
+            n_episodes=episodes, 
+            target_average=-10,
+            max_t=200,
+            gamma=genome['gamma'],
+            learning_rate=genome['learningRate'],
+            fc1_units=genome['fc1_units'])
+
+    def get_evolver_config(self):
+        return {
+            'episodes': 20,
+            'populationSize': 25,
+            'retainSize': 5,
+            'mutateOneGeneRandom': 5,
+            'mutateTwoGenesRandom': 5,
+            'crossoverOneGene': 5,
+            'crossOverTwoGenes': 5,
+            'mutateOneGeneClose': 5,
+            'generations': 2,
+            'randomSeed': 1,
+            'allPossibleGenesSimple': {
+                'learningRate': [0.01, 0.001, 0.002, 0.003, 0.004, 0.0005],
+                'gamma':        [0.90, 0.92, 0.94, 0.96, 0.98, 0.9, 0.995, 1.0],
+                'fc1_units':    [16,32,64,128,256,512],
+            },
+        }
+
+    def run(self, runner):
+        return runner.run_single_probability_trajectory()
+
+if __name__ == "__main__":
+    run = AcrobotREINFORCE()
+    run.command(sys.argv[1])
--- run_experiment_helper.py
+++ run_experiment_helper.py
@@ -27,7 +27,7 @@ class RunExperiment():
         env = gym.make(self.gym_name) # Make the gym
         agent = self.get_agent(AgentConfig(env=env)) # Create the agent
         runner = Runner(agent) # Create a runner that runs the agent in the environment    
-        runner.enjoy_checkpoint('Checkpoints/{}_evo.ch'.format(self.gym_name)) # Run the agent
+        runner.enjoy_checkpoint('Checkpoints/{}_evo.ch'.format(self.gym_name.lower())) # Run the agent
 
         env.close()
 
@@ -37,8 +37,8 @@ class RunExperiment():
 
         agent = self.get_agent(self.get_agent_train_config(env)) # Create an agent that solves the environment:
 
-        runner = Runner(agent, save_best_score='Checkpoints/{}_train.ch'.format(self.gym_name)) # Create a runner that runs the agent in the environment
-        score, checkpoint = runner.run_agent() # Run the agent
+        runner = Runner(agent, save_best_score='Checkpoints/{}_train.ch'.format(self.gym_name.lower())) # Create a runner that runs the agent in the environment
+        score, checkpoint = self.run(runner) # Run the agent
         score.visualize() # Visualize the score
 
         env.close()
@@ -47,60 +47,26 @@ class RunExperiment():
 
         env = gym.make(self.gym_name) # make gym
 
-        config = AgentConfig(
-            env=env,
-            n_episodes=episodes, 
-            target_average=475,
-            update_every=genome['updateEvery'],
-            batch_size=genome['batchSize'],
-            gamma=genome['gamma'],
-            learning_rate=genome['learningRate'],
-            fc1_units=genome['fc1_units'],
-            fc2_units=genome['fc2_units'],
-            tau=genome['tau'],
-            memory_size=genome['memory_size'],
-            deepq_double_learning=True,
-            deepq_dueling_networks=True,
-            per_active=True)
-        agent = DQNAgent(config) # Create an agent that solves the environment:
+        agent = self.get_agent(self.get_agent_evolve_config(env, genome, episodes)) 
         
         runner = Runner(agent, verbose=1, pbar=pbar) # Create a runner that runs the agent in the environment
-        score, checkpoint = runner.run_agent()  # Run the agent
+        score, checkpoint = self.run(runner)  # Run the agent
         env.close() # Close
 
         return score.best_score, checkpoint
 
     def save_checkpoint(self, checkpoint, filepath):
+
         torch.save(checkpoint, filepath)
 
     def evolve_agent(self):
 
-        evolverConfig = {
-            'score_genome': score_genome,
-            'save_checkpoint': save_checkpoint,
-            'save_filepath':'Checkpoints/{}_evo.ch'.format(self.gym_name),
-            'GPUDevices': None,
-            'episodes': 1000,
-            'populationSize': 25,
-            'retainSize': 5,
-            'mutateOneGeneRandom': 5,
-            'mutateTwoGenesRandom': 5,
-            'crossoverOneGene': 5,
-            'crossOverTwoGenes': 5,
-            'mutateOneGeneClose': 5,
-            'generations': 2,
-            'randomSeed': 1,
-            'allPossibleGenesSimple': {
-                'learningRate': [0.01, 0.001, 0.002, 0.003, 0.004, 0.0001, 0.0005],
-                'batchSize':    [32,64,128,256,512],
-                'gamma':        [0.90, 0.92, 0.94, 0.96, 0.98, 0.9, 0.995],
-                'updateEvery':  [1,2,3,4],
-                'fc1_units':    [64,128,256,512],
-                'fc2_units':    [64,128,256,512],
-                'memory_size':  [int(1e4), int(1e5), int(1e6)],
-                'tau':          [1e-2, 1e-3, 1e-4],
-            },
-        }
+        evolverConfig = self.get_evolver_config()
+
+        evolverConfig['score_genome'] = self.score_genome
+        evolverConfig['save_checkpoint'] = self.save_checkpoint
+        evolverConfig['save_filepath'] = 'Checkpoints/{}_evo.ch'.format(self.gym_name.lower())
+        evolverConfig['GPUDevices'] = None
 
         evolver_test = evolver(evolverConfig)
         evolver_test.start()
@@ -112,3 +78,5 @@ class RunExperiment():
             self.train_agent()
         if arg == "-evolve":
             self.evolve_agent()
+        if arg == "-enjoy":
+            self.enjoy_agent()
